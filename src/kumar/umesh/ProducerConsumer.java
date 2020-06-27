65b1f8c2d595151c29ddee9b3042e7ab240dd102
Kumar <umesh.kumar@cox.com>
2020-06-27 12:38:44 -0400

    Renamed main class

--- src/kumar/umesh/ProducerConsumer.java
+++ src/kumar/umesh/ProducerConsumer.java
@@ -1,6 +1,6 @@
 package kumar.umesh;
 
-public class Main {
+public class ProducerConsumer {
 
 	public static void main(String [] args) {
 		System.out.println("This is a test producer - consumer problem");
